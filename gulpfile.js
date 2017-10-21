b889140f6d6abe9cad01b836951a64677bbfb053
alsoknownasdrew <andriy.frankevych@gmail.com>
2017-10-21 21:27:19 +0200

    Set up Gulp scripts concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 var config = {
     sassPath: './scss/**/*.scss'
@@ -18,4 +19,14 @@ gulp.task('watch', function(){
     gulp.watch(config.sassPath, ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('scripts', function(){
+    gulp.src([
+        './node_modules/jquery/dist/jquery-3.2.1.slim.min.js',
+        './node_modules/popper.js/dist/popper.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('default', ['scripts', 'sass', 'watch']);
