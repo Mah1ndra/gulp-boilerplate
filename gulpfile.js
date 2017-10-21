537560fdd0f2c5d0f4e7f0a78be2d4a8b123c867
alsoknownasdrew <andriy.frankevych@gmail.com>
2017-10-21 18:41:14 +0200

    Set up Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function(){
-   //gulp stuff
+    gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
 });
