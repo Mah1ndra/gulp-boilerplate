030620962b158a9f24084af1be65ecc94e166edc
alsoknownasdrew <andriy.frankevych@gmail.com>
2017-10-21 19:31:11 +0200

    Setup Gulp Sass workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function(){
-    gulp.src('./scss/**/*.scss')
+var config = {
+    sassPath: './scss/**/*.scss'
+};
+
+gulp.task('sass', function(){
+    gulp.src(config.sassPath)
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css'));
 });
+
+gulp.task('watch', function(){
+    gulp.watch(config.sassPath, ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
