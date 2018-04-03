9b15c5f2f6871bea24bf5cedc897734b32473291
alsoknownasdrew <andriy.frankevych@gmail.com>
2018-04-03 11:57:28 +0200

    Gulpfile refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,95 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
-var plumber = require('gulp-plumber');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const minifyCss = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
-var config = {
-    sassPath: './src/scss/**/*.scss'
+
+const config = {
+    styles: {
+        partials: './src/scss/partials/*.scss',
+        src: './src/scss/*.scss',
+        build: './public/assets/css/'
+    },
+    scripts: {
+        src: './src/js/*.js',
+        build: './public/assets/js/'
+    },
+    fonts: {
+        build: './public/assets/fonts/'
+    },
+    images: {
+        src: './src/img/**/*',
+        build: './public/assets/img/'
+    }
 };
 
+//Run styles and srripts tasks on changes in src directories
 gulp.task('watch', function(){
-    gulp.watch(config.sassPath, ['sass']);
+    gulp.watch([
+        config.styles.src,
+        config.scripts.src
+    ], [
+        'styles',
+        'scripts'
+    ]);
 });
 
-gulp.task('sass', function(){
+//Compile scss to css, concat and minify styles
+gulp.task('styles', function (){
     gulp.src([
         './node_modules/bootstrap/dist/css/bootstrap.min.css',
         './node_modules/font-awesome/css/font-awesome.min.css',
-        config.sassPath
+        config.styles.src
     ])
-        .pipe(plumber(function(error) {
-            console.log(error.toString());
-            this.emit('end');
-        }))
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(concat('main.min.css'))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(concat('main.min.css'))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(minifyCss({
+            compatibility: 'ie9'
+        }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest(config.styles.build));
 });
 
-gulp.task('fonts', function(){
+//Move fonts into public dir
+gulp.task('fonts', function (){
     gulp.src([
-        './node_modules/bootstrap/dist/fonts/*',
         './node_modules/font-awesome/fonts/*'
     ])
-    .pipe(gulp.dest('./dist/fonts'));
+        .pipe(gulp.dest(config.fonts.build));
 });
 
-gulp.task('scripts', function(){
+//Concat and minify js
+gulp.task('scripts', function (){
     gulp.src([
-        './node_modules/jquery/dist/jquery.slim.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './src/js/app.js'
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        config.scripts.src
     ])
-        .pipe(plumber(function(error) {
-            console.log(error.toString());
-            this.emit('end');
-        }))
+        .pipe(plumber())
         .pipe(concat('app.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest(config.scripts.build));
 });
 
-gulp.task('default', ['scripts', 'sass', 'fonts']);
+//Optimize images
+gulp.task('images', function() {
+    gulp.src([
+        config.images.src
+    ])
+        .pipe(plumber())
+        .pipe(imagemin())
+        .pipe(gulp.dest(config.images.build));
+});
+
+gulp.task('default', ['styles', 'scripts', 'fonts', 'images']);
